a44fdf006fe0c167a80f7de7c109b4169cf6f1d4
Mark Spanbroek <mark@spanbroek.net>
2021-01-12 21:29:09 +0100

    Set NeoPixel to the color of gooseberry

--- src/main.nim
+++ src/main.nim
@@ -1,10 +1,18 @@
 import pkg/nim-arduino/arduino
+import pkg/nim-neopixel/neopixel
+
+let pixel = initNeoPixel(1, 11)
 
 setup:
   pinMode(LED_BUILTIN, OUTPUT)
+  pixel.begin()
+  pixel.setPixelColor(0, 2, 4, 0, 0)
+  pixel.show()
 
 loop:
   digitalWrite(LED_BUILTIN, HIGH)
   delay(1000)
   digitalWrite(LED_BUILTIN, LOW)
   delay(1000)
+  pixel.clear()
+  pixel.show()
