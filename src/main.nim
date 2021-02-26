a3a1b30cb913ef9cf1134802add49181e9bfaf63
Mark Spanbroek <mark@spanbroek.net>
2021-02-26 07:09:49 +0100

    Use ILI9321 display

--- src/main.nim
+++ src/main.nim
@@ -1,13 +1,18 @@
 import pkg/nim-arduino/arduino
 import pkg/nim-neopixel/neopixel
+import pkg/nim-ili9341/ili9341
 
 let pixel = initNeoPixel(1, 11)
+let tft = initIli9321(9, 10)
 
 setup:
   pinMode(LED_BUILTIN, OUTPUT)
   pixel.begin()
   pixel.setPixelColor(0, 2, 4, 0, 0)
   pixel.show()
+  tft.begin()
+  tft.setRotation(1)
+  tft.println("Hello World!")
 
 loop:
   digitalWrite(LED_BUILTIN, HIGH)
